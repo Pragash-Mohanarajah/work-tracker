1604e11db93ee39d0e5ebf72fdbe0c734d87a9a8
Pragash Mohanarajah <mpragash1706@gmail.com>
2026-04-05 14:00:13 +0100

    chore: correct import route for work tracker section function

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 const { fetchStats } = require("./fetchStats");
+const { buildWorkTrackerSection } = require("./sections");
 const {
-  buildWorkTrackerSection,
   generatePieChartSvg,
   generateBranchActivitySvg,
   generateRadarChartSvg,
